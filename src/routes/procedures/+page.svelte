6852d38ab28cfa1d2b0b4d6190c10c2e3e2686c7
Aetheris743 <45411688+Aetheris743@users.noreply.github.com>
2023-10-05 16:59:45 -0600

    Small update

--- src/routes/procedures/+page.svelte
+++ src/routes/procedures/+page.svelte
@@ -2,53 +2,26 @@
 	import { Breadcrumb, BreadcrumbItem, Heading, Span } from 'flowbite-svelte';
 	import { Timeline, TimelineItem, Button, Card } from 'flowbite-svelte';
 	import { ArrowRightOutline, PlusSolid } from 'flowbite-svelte-icons';
+
+	let prodecureNames = [
+		'Test Procedure 1',
+		'How to make a sandwich',
+		'Emergency Snack Procedure',
+		'Deploy Parachute',
+		'Do EVA',
+		'Test Procedure 5'
+	];
 </script>
 
 <div class="w-fit h-fit m-4">
-	<Breadcrumb solid>
+	<Breadcrumb solid class="mb-4">
 		<BreadcrumbItem href="/" home>Home</BreadcrumbItem>
-		<BreadcrumbItem href="/">TestProcedure</BreadcrumbItem>
+		<BreadcrumbItem href="/procedures">Procedures</BreadcrumbItem>
 	</Breadcrumb>
-	<Card>
-		<Heading tag="h2" class="mb-3">Procedure Name</Heading>
-		<Timeline>
-			<TimelineItem title="Install Jetpack" date="Step 1">
-				<p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
-					Update all of the procedure here to see what can be done to improve the expereince of the <span
-						class="text-primary-700">test</span
-					>
-				</p>
-				<Button color="alternative">Battery &lt 20%</Button>
-				<Button color="alternative">Oxygen &gt 50%</Button>
-				<Button color="alternative">New Condition<PlusSolid class="w-3 h-3 ml-2" /></Button>
-			</TimelineItem>
-			<TimelineItem title="Launch Missiles" date="Step 2">
-				<p class="text-base font-normal text-gray-500 dark:text-gray-400 mb-4">Here is another test</p>
-				<Button color="alternative">New Condition<PlusSolid class="w-3 h-3 ml-2" /></Button>
-			</TimelineItem>
-			<TimelineItem title="Prepare Tofu" date="Step 3">
-				<p class="text-base font-normal text-gray-500 dark:text-gray-400 mb-4">
-					This is a long and complicated procedure with multiple substeps
-				</p>
-                <Timeline>
-                    <TimelineItem date="Step 3.1">
-                        <p class="text-base font-normal text-gray-500 dark:text-gray-400 mb-4">
-                            This is a long and complicated procedure with multiple substeps
-                        </p>
-                    </TimelineItem>
-                    <TimelineItem date="Step 3.2">
-                        <p class="text-base font-normal text-gray-500 dark:text-gray-400 mb-4">
-                            This is a long and complicated procedure with multiple substeps
-                        </p>
-                    </TimelineItem>
-                    <TimelineItem date="Step 3.3">
-                        <p class="text-base font-normal text-gray-500 dark:text-gray-400 mb-4">
-                            This is a long and complicated procedure with multiple substeps
-                        </p>
-                    </TimelineItem>
-                </Timeline>
-				<Button color="alternative">New Condition<PlusSolid class="w-3 h-3 ml-2" /></Button>
-			</TimelineItem>
-		</Timeline>
-	</Card>
+	<Heading tag="h2" class="mb-3">Procedures</Heading>
+	<div class="flex flex-row flex-wrap">
+		{#each prodecureNames as prcedure}
+			<Button href="/procedures/{prcedure}" class="m-1 flex" color="alternative">{prcedure}</Button>
+		{/each}
+	</div>
 </div>
--- src/routes/procedures/[procedureName]/procedure.svelte
+++ src/routes/procedures/[procedureName]/procedure.svelte
@@ -1,71 +1,74 @@
 <script>
 	import { Timeline, TimelineItem, Button, Card, Heading } from 'flowbite-svelte';
-    import { ArrowRightOutline, PlusSolid } from 'flowbite-svelte-icons';
+	import { ArrowRightOutline, PlusSolid } from 'flowbite-svelte-icons';
 	import ProcedureStep from './ProcedureStep.svelte';
 
-    export let name;
+	export let name;
 
-    // let testProceduralProcedure = {
-    //     title: "Install Jetpack",
-    //     date: "Step 3",
-    //     description: "Update all of the procedure here to see what can be done to improve the expereince of the",
-    //     substeps: [
-    //         {
-    //             title: "Install Jetpack",
-    //             date: "Step 1",
-    //             description: "Update all of the procedure here to see what can be done to improve the expereince of the"
-    //         },
-    //         {
-    //             title: "Install Jetpack",
-    //             date: "Step 1",
-    //             description: "Update all of the procedure here to see what can be done to improve the expereince of the"
-    //         },
-    //         {
-    //             title: "Install Jetpack",
-    //             date: "Step 1",
-    //             description: "Update all of the procedure here to see what can be done to improve the expereince of the"
-    //         }
-    //     ]
-    // }
-    let allSteps = [
-        {
-            title: "Install Jetpack",
-            description: "Update all of the procedure here to see what can be done to improve the expereince of the",
-            links: ["Battery < 20%", "Oxygen > 50%"]
-        },
-        {
-            title: "Launch Missiles",
-            description: "Update all of the procedure here to see what can be done to improve the expereince of the"
-        },
-        {
-            title: "Propare Tofu",
-            description: "Update all of the procedure here to see what can be done to improve the expereince of the",
-            substeps: [
-                {
-                    title: "Boil Water",
-                    date: "Step 1",
-                    description: "Update all of the procedure here to see what can be done to improve the expereince of the"
-                },
-                {
-                    title: "[REDACTED]",
-                    date: "Step 1",
-                    description: "[REDACTED]"
-                },
-                {
-                    title: "Serve",
-                    date: "Step 1",
-                    description: "Update all of the procedure here to see what can be done to improve the expereince of the"
-                }
-            ]
-        }
-    ]
+	// let testProceduralProcedure = {
+	//     title: "Install Jetpack",
+	//     date: "Step 3",
+	//     description: "Update all of the procedure here to see what can be done to improve the expereince of the",
+	//     substeps: [
+	//         {
+	//             title: "Install Jetpack",
+	//             date: "Step 1",
+	//             description: "Update all of the procedure here to see what can be done to improve the expereince of the"
+	//         },
+	//         {
+	//             title: "Install Jetpack",
+	//             date: "Step 1",
+	//             description: "Update all of the procedure here to see what can be done to improve the expereince of the"
+	//         },
+	//         {
+	//             title: "Install Jetpack",
+	//             date: "Step 1",
+	//             description: "Update all of the procedure here to see what can be done to improve the expereince of the"
+	//         }
+	//     ]
+	// }
+	let allSteps = [
+		{
+			title: 'Install Jetpack',
+			description:
+				'Update all of the procedure here to see what can be done to improve the expereince of the',
+			links: ['Battery < 20%', 'Oxygen > 50%']
+		},
+		{
+			title: 'Launch Missiles',
+			description:
+				'Update all of the procedure here to see what can be done to improve the expereince of the'
+		},
+		{
+			title: 'Propare Tofu',
+			description:
+				'Update all of the procedure here to see what can be done to improve the expereince of the',
+			substeps: [
+				{
+					title: 'Boil Water',
+					date: 'Step 1',
+					description:
+						'Update all of the procedure here to see what can be done to improve the expereince of the'
+				},
+				{
+					title: '[REDACTED]',
+					date: 'Step 1',
+					description: '🔥'
+				},
+				{
+					title: 'Serve',
+					date: 'Step 1',
+					description:
+						'Update all of the procedure here to see what can be done to improve the expereince of the'
+				}
+			]
+		}
+	];
 </script>
 
-<Card>
-	<Heading tag="h2" class="mb-3">{name}</Heading>
-	<Timeline>
-        {#each allSteps as step}
-            <ProcedureStep {...step} date="Step {allSteps.indexOf(step) + 1}" />
-        {/each}
-	</Timeline>
-</Card>
+<Heading tag="h2" class="mb-3">{name}</Heading>
+<Timeline>
+	{#each allSteps as step}
+		<ProcedureStep {...step} date="Step {allSteps.indexOf(step) + 1}" />
+	{/each}
+</Timeline>
