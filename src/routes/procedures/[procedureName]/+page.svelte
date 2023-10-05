c6370a2ef577ec190478ae1faa030a37e481a3a7
Aetheris743 <45411688+Aetheris743@users.noreply.github.com>
2023-10-05 13:54:03 -0600

    Moved Procedure to a Component

--- src/routes/procedures/[procedureName]/+page.svelte
+++ src/routes/procedures/[procedureName]/+page.svelte
@@ -1,9 +1,16 @@
 <script>
+	import { Breadcrumb, BreadcrumbItem, Heading, Span } from 'flowbite-svelte';
+	import Procedure from './procedure.svelte';
+
     /** @type {import('./$types').PageData} */
     export let data;
 </script>
 
-
-<div>
-    {data.name}
+<div class="w-fit h-fit m-4">
+	<Breadcrumb solid class="mb-4">
+		<BreadcrumbItem href="/" home>Home</BreadcrumbItem>
+		<BreadcrumbItem href="/procedures">Procedures</BreadcrumbItem>
+		<BreadcrumbItem href="/procedures/{data.name}">{data.name}</BreadcrumbItem>
+	</Breadcrumb>
+    <Procedure name={data.name} />
 </div>
---
+++ src/routes/procedures/[procedureName]/procedure.svelte
@@ -0,0 +1,50 @@
+<script>    
+	import { Timeline, TimelineItem, Button, Card, Heading } from 'flowbite-svelte';
+	import { ArrowRightOutline, PlusSolid } from 'flowbite-svelte-icons';
+
+    export let name;
+</script>
+
+
+<Card>
+    <Heading tag="h2" class="mb-3">{name}</Heading>
+    <Timeline>
+        <TimelineItem title="Install Jetpack" date="Step 1">
+            <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+                Update all of the procedure here to see what can be done to improve the expereince of the <span
+                    class="text-primary-700">test</span
+                >
+            </p>
+            <Button color="alternative">Battery &lt 20%</Button>
+            <Button color="alternative">Oxygen &gt 50%</Button>
+            <Button color="alternative">New Condition<PlusSolid class="w-3 h-3 ml-2" /></Button>
+        </TimelineItem>
+        <TimelineItem title="Launch Missiles" date="Step 2">
+            <p class="text-base font-normal text-gray-500 dark:text-gray-400 mb-4">Here is another test</p>
+            <Button color="alternative">New Condition<PlusSolid class="w-3 h-3 ml-2" /></Button>
+        </TimelineItem>
+        <TimelineItem title="Prepare Tofu" date="Step 3">
+            <p class="text-base font-normal text-gray-500 dark:text-gray-400 mb-4">
+                This is a long and complicated procedure with multiple substeps
+            </p>
+            <Timeline>
+                <TimelineItem date="Step 3.1">
+                    <p class="text-base font-normal text-gray-500 dark:text-gray-400 mb-4">
+                        This is a long and complicated procedure with multiple substeps
+                    </p>
+                </TimelineItem>
+                <TimelineItem date="Step 3.2">
+                    <p class="text-base font-normal text-gray-500 dark:text-gray-400 mb-4">
+                        This is a long and complicated procedure with multiple substeps
+                    </p>
+                </TimelineItem>
+                <TimelineItem date="Step 3.3">
+                    <p class="text-base font-normal text-gray-500 dark:text-gray-400 mb-4">
+                        This is a long and complicated procedure with multiple substeps
+                    </p>
+                </TimelineItem>
+            </Timeline>
+            <Button color="alternative">New Condition<PlusSolid class="w-3 h-3 ml-2" /></Button>
+        </TimelineItem>
+    </Timeline>
+</Card>
