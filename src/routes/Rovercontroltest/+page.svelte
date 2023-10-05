cce83537ef670ca318d8cd8f77c85ff1723fd671
David <david@lotaris.org>
2023-10-04 20:08:02 -0600

    One button works

--- src/routes/Rovercontroltest/+page.svelte
+++ src/routes/Rovercontroltest/+page.svelte
@@ -1,14 +1,55 @@
 <script>
 	import { Button } from 'flowbite-svelte';
-    let clicked=false;
+	import { onMount } from 'svelte';
+	let clicked = false;
+	let poll;
 	/** @type {import('./$types').PageData} */
 	export let data;
 
-   
+	// window.ongamepadconnected=(e) => {console.log(e);}
+	onMount(() => {
+		console.log(navigator.getGamepads());
+	});
+
+	function printconnectedgamepads() {
+		console.log(navigator.getGamepads()[0].buttons);
+	}
+
+	function plugIn() {
+		doFrame();
+	}
+	function doFrame() {
+		var connectedController = navigator.getGamepads()[0];
+		if (connectedController == null) {
+			return;
+		}
+		var buttons = connectedController.buttons;
+		for (let buttonindex = 0; buttonindex < buttons.length; buttonindex++) {
+			const element = buttons[buttonindex];
+			if (buttonindex == 0) {
+				if (element.pressed) {
+					clicked = true;
+				} else {
+					clicked = false;
+				}
+			}
+		}
+
+		poll = requestAnimationFrame(doFrame);
+	}
+	function unPlug() {}
 </script>
 
-<div on:mousedown={() => {clicked=true;}} on:mouseup={() => {clicked=false;}}>
-	<Button color={clicked ? "primary" : 'alternative'} >
-        X
-    </Button>
+<div
+	on:mousedown={() => {
+		printconnectedgamepads();
+		clicked = true;
+	}}
+	on:mouseup={() => {
+		clicked = false;
+	}}
+>
+	<Button color={clicked ? 'primary' : 'alternative'}>X</Button>
 </div>
+
+<svelte:window on:gamepadconnected={plugIn} on:gamepaddisconnected={unPlug} />
