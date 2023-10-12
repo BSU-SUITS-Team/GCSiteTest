9bdcf383d93f3badaf8a0221e0a8aee407ba8039
Aetheris743 <45411688+Aetheris743@users.noreply.github.com>
2023-10-12 10:06:23 -0600

    Placeholder

--- src/routes/Sidebar.svelte
+++ src/routes/Sidebar.svelte
@@ -65,6 +65,13 @@
 					/>
 				</svelte:fragment>
 			</SidebarItem> -->
+			<SidebarItem label="Map" href="/map">
+				<svelte:fragment slot="icon">
+					<MapPinAltSolid
+						class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+					/>
+				</svelte:fragment>
+			</SidebarItem>
 		</SidebarGroup>
 	</SidebarWrapper>
 </Sidebar>
---
+++ src/routes/map/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+</script>
+
+<div>
+    This is a placeholder for the map page.
+</div>
