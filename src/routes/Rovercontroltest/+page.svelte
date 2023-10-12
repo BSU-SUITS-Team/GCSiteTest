c56375e7d93402979021b1c9454c65aa26e89ceb
David <david@lotaris.org>
2023-10-12 10:30:14 -0600

    Working Prototype
    All buttons and joysticks on controller mapped to webpage

--- src/routes/Rovercontroltest/+page.svelte
+++ src/routes/Rovercontroltest/+page.svelte
@@ -1,14 +1,36 @@
 <script>
 	import { Button } from 'flowbite-svelte';
 	import { onMount } from 'svelte';
-	let clicked = false;
+	import Joystick from './Joystick.svelte';
+	let clickedButtons = [];
+	const buttonNames = [
+		'X',
+		'O',
+		'□',
+		'△',
+		'LB',
+		'RB',
+		'LT',
+		'RT',
+		'▫▫▫',
+		'▤',
+		'L3',
+		'R3',
+		'↑',
+		'↓',
+		'←',
+		'→',
+		'doNotPress',
+		'Gamepad'
+	];
+	let joystickLeft = [];
+	let joystickRight = [];
+	for (let i = 0; i < 18; i++) {
+		clickedButtons.push(false);
+	}
 	let poll;
-	
 
 	// window.ongamepadconnected=(e) => {console.log(e);}
-	onMount(() => {
-		console.log(navigator.getGamepads());
-	});
 
 	function printconnectedgamepads() {
 		console.log(navigator.getGamepads()[0].buttons);
@@ -22,15 +44,17 @@
 		if (connectedController == null) {
 			return;
 		}
+		let joysticks = connectedController.axes;
+		joystickLeft = joysticks.slice(0, 2);
+		joystickRight = joysticks.slice(2, 4);
 		var buttons = connectedController.buttons;
 		for (let buttonindex = 0; buttonindex < buttons.length; buttonindex++) {
-			const element = buttons[buttonindex];
-			if (buttonindex == 0) {
-				if (element.pressed) {
-					clicked = true;
-				} else {
-					clicked = false;
-				}
+			const button = buttons[buttonindex];
+
+			if (button.pressed) {
+				clickedButtons[buttonindex] = true;
+			} else {
+				clickedButtons[buttonindex] = false;
 			}
 		}
 
@@ -40,16 +64,15 @@
 </script>
 
 <!-- svelte-ignore a11y-no-static-element-interactions -->
-<div
-	on:mousedown={() => {
-		printconnectedgamepads();
-		clicked = true;
-	}}
-	on:mouseup={() => {
-		clicked = false;
-	}}
->
-	<Button color={clicked ? 'primary' : 'alternative'}>X</Button>
+<div>
+	<!-- <Button color={clickedButtons[0] ? 'primary' : 'alternative'}>X</Button> -->
+	{#each buttonNames as button}
+		<Button color={clickedButtons[buttonNames.indexOf(button)] ? 'primary' : 'alternative'}
+			>{button}</Button
+		>
+	{/each}
+	<Joystick  joystickvalue={joystickLeft} name = "left joystick"></Joystick>
+	<Joystick  joystickvalue={joystickRight} name = "right Joystick"></Joystick>
 </div>
 
 <svelte:window on:gamepadconnected={plugIn} on:gamepaddisconnected={unPlug} />
---
+++ src/routes/Rovercontroltest/Joystick.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let joystickvalue;
+    export let name = "joystick";
+</script>
+<div>
+    <h1>{name}</h1>
+    <div>
+        x: {joystickvalue[0]}
+        y: {joystickvalue[1]}
+    </div>
+</div>
