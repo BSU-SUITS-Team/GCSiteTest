0aa61e2b996fe69f6c610d307b3273846fea3da1
Aetheris743 <45411688+Aetheris743@users.noreply.github.com>
2023-10-15 14:30:43 -0600

    Removed duplicate

--- src/routes/Sidebar.svelte
+++ src/routes/Sidebar.svelte
@@ -35,15 +35,6 @@
                 </SidebarDropdownItem>
                 <SidebarDropdownItem label="Astroanut" href="/astronaut" active={activeUrl == "/astronaut"}/>
             </SidebarDropdownWrapper>
-            <SidebarItem
-                label="Map"
-            >
-                <svelte:fragment slot="icon">
-                    <MapPinAltSolid
-                        class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                    />
-                </svelte:fragment>
-            </SidebarItem>
 			<SidebarItem label="Events" {spanClass}>
 				<svelte:fragment slot="icon">
 					<ExclamationCircleSolid
