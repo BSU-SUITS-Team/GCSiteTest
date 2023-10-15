6f705704a8eeb563b3608ff91899b2d9877fd4f3
Aetheris743 <45411688+Aetheris743@users.noreply.github.com>
2023-10-15 15:24:29 -0600

    Update BoxButton.svelte

--- src/routes/map/BoxButton.svelte
+++ src/routes/map/BoxButton.svelte
@@ -9,7 +9,7 @@
 <div>
 	<DrawSquareSolid 
 		on:click={() => {onclick(); toggled = !toggled}}
-		class="m-2 w-5 h-5 text-{toggled ? 'primary' : 'gray'}-500 dark:text-{toggled ? 'primary' : 'gray'}-400 transition duration-75 focus:outline-none"
+		class="m-2 w-5 h-5 text-{toggled ? 'black' : 'gray-400'} dark:text-{toggled ? 'white' : 'gray-400'} transition duration-75 focus:outline-none"
 		style="{move ? 'transform: translate3d(-95%, -150%, 0);' : ''}"
 	/>
 </div>
