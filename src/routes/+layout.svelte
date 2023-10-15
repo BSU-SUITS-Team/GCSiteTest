130f233f144d5434aa29472440b44b6c9a7f3487
Aetheris743 <45411688+Aetheris743@users.noreply.github.com>
2023-10-15 13:38:49 -0600

    Map is big now

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,8 @@
 	import { TrashBinOutline, FileEditSolid } from 'flowbite-svelte-icons';
 	import Graph from './graph.svelte';
 	import TinyGraph from './TinyGraph.svelte';
+
+	$: hasSideBar = Object.keys($keepables).length > 0 || Object.keys($graphdata).length > 0;
 </script>
 
 <div class="app h-screen dark:bg-gray-900 flex overflow-hidden">
@@ -26,48 +28,55 @@
 		</div>
 	</aside>
 
-	<main class="flex flex-col flex-1 ml-72 overflow-y-auto mr-72">
+	<main class="flex flex-col flex-1 ml-64 overflow-y-auto {hasSideBar ? 'mr-72' : ''}">
 		<slot />
 	</main>
+	{#if hasSideBar}
+		<div
+			class="absolute right-0 h-full p-8 bg-white dark:bg-gray-900 overflow-y-auto"
+			style="width: 22rem;"
+		>
+			{#each new Set([...Object.keys($keepables), ...Object.keys($graphdata)]) as label}
+				<div class="border-b flex-row flex pb-2 mb-1 dark:border-gray-700">
+					<Heading tag="h4">{label}</Heading>
+					<FileEditSolid class="dark:text-gray-400 mr-2 h-7 text-gray-800" href="/rover" />
+					<TrashBinOutline class="dark:text-gray-400 h-7 text-gray-800" />
+				</div>
 
-	<div class="absolute right-0 h-full p-8 bg-white dark:bg-gray-900 overflow-y-auto" style="width: 22rem;">
-		{#each new Set([...Object.keys($keepables), ...Object.keys($graphdata)]) as label}
-			<div class="border-b flex-row flex pb-2 mb-1 dark:border-gray-700">
-				<Heading tag="h4">{label}</Heading>
-				<FileEditSolid class="dark:text-gray-400 mr-2 h-7 text-gray-800" href="/rover" />
-				<TrashBinOutline class="dark:text-gray-400 h-7 text-gray-800" />
-			</div>
+				{#if $keepables[label]}
+					<div class="flex justify-left pr-2 pl-2 flex-wrap">
+						{#each $keepables[label] as item}
+							<div class="p-1">
+								<Button
+									color="alternative"
+									on:click={() => keepables.removeElement(label, item[0])}
+								>
+									{item[0]}&nbsp
+									<Span highlight>{item[1]}</Span>
+								</Button>
+							</div>
+						{/each}
+					</div>
+				{/if}
 
-			{#if $keepables[label]}
-				<div class="flex justify-left pr-2 pl-2 flex-wrap">
-					{#each $keepables[label] as item}
-						<div class="p-1">
-							<Button color="alternative" on:click={() => keepables.removeElement(label, item[0])}>
-								{item[0]}&nbsp
-								<Span highlight>{item[1]}</Span>
-							</Button>
+				{#if $graphdata[label]}
+					{#each Object.keys($graphdata[label]) as graph}
+						<div
+							class="h-52 flex p-3"
+							on:click={() => graphdata.removeGraph(label, graph)}
+							role="button"
+							tabindex="0"
+							on:keydown={() => graphdata.removeGraph(label, graph)}
+						>
+							<TinyGraph
+								graphdata={$graphdata[label][graph]}
+								name={graph}
+								status={$graphdata[label][graph][$graphdata[label][graph].length - 1]}
+							/>
 						</div>
 					{/each}
-				</div>
-			{/if}
-
-			{#if $graphdata[label]}
-				{#each Object.keys($graphdata[label]) as graph}
-					<div
-						class="h-52 flex p-3"
-						on:click={() => graphdata.removeGraph(label, graph)}
-						role="button"
-						tabindex="0"
-						on:keydown={() => graphdata.removeGraph(label, graph)}
-					>
-						<TinyGraph
-							graphdata={$graphdata[label][graph]}
-							name={graph}
-							status={$graphdata[label][graph][$graphdata[label][graph].length - 1]}
-						/>
-					</div>
-				{/each}
-			{/if}
-		{/each}
-	</div>
+				{/if}
+			{/each}
+		</div>
+	{/if}
 </div>
--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -1,24 +1,16 @@
 <script>
-	import Panview from "./Panview.svelte";
-    import img from '$lib/images/test.svg';
-	import { MapPinAltSolid } from "flowbite-svelte-icons";
-	import PinButton from "./PinButton.svelte";
-	import { Heading } from "flowbite-svelte";
+	import Panview from './Panview.svelte';
+	import img from '$lib/images/test.svg';
+	import { MapPinAltSolid } from 'flowbite-svelte-icons';
+	import PinButton from './PinButton.svelte';
+	import { Heading } from 'flowbite-svelte';
 </script>
 
-<div class="pr-96 h-full">
-    <div class="flex justify-center items-center h-full w-fit flex-col">
-        <Heading class="pb-3">Map</Heading>
-        <div class="rounded-lg overflow-clip shadow-xl flex" style="width: 40rem; height: 40rem;">
-            <Panview 
-                image={img}
-                initalSize=10
-                minScale=1
-                maxScale=20
-                xRange={[-1000, 0]}
-                yRange={[-1000, 0]}
-            >
-            </Panview>
-        </div>
-    </div>
-</div>
+<Panview
+	image={img}
+	initalSize="10"
+	minScale="3"
+	maxScale="20"
+	xRange={[-1000, 0]}
+	yRange={[-1000, 0]}
+/>
