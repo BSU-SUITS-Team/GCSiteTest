5420f454bdce0b0b9ebcd0ae12ea5adf2e87432d
Aetheris743 <45411688+Aetheris743@users.noreply.github.com>
2023-10-15 11:45:04 -0600

    Made Progress

--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -1,8 +1,22 @@
 <script>
 	import Panview from "./Panview.svelte";
-    import img from '$lib/images/MapTest.png';
+    import img from '$lib/images/test.svg';
+	import { MapPinAltSolid } from "flowbite-svelte-icons";
+	import PinButton from "./PinButton.svelte";
 </script>
 
-
-<Panview image={img}>
-</Panview>
+<div class="pr-96 h-full">
+    <div class="flex justify-center items-center h-full w-fit flex-col">
+        <div class="mb-4 p-2 shadow-xl rounded-lg flex-row flex">
+            <PinButton/>
+            <PinButton/>
+            <PinButton/>
+            <PinButton/>
+            <PinButton/>
+        </div>
+        <div class="rounded-lg overflow-clip shadow-xl flex" style="width: 40rem; height: 40rem;">
+            <Panview image={img}>
+            </Panview>
+        </div>
+    </div>
+</div>
---
+++ src/routes/map/PinButton.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { MapPinAltSolid } from 'flowbite-svelte-icons';
+</script>
+
+<MapPinAltSolid
+	class="m-1 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+/>
