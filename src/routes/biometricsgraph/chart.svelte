099ca3e34533bc75baa819eb97f85998dbb51dc3
Aetheris743 <45411688+Aetheris743@users.noreply.github.com>
2023-10-18 13:48:03 -0600

    Update chart.svelte

--- src/routes/biometricsgraph/chart.svelte
+++ src/routes/biometricsgraph/chart.svelte
@@ -1,16 +1,23 @@
 <script>
-	export let fullRange = 10;
+	export let fullRange = [0, 10]; // example of a range instead of a singular value
 	export let warnRange = [2.5, 7.5];
 	export let nominalRange = [3.5, 6.5];
 	export let value = 5;
 
-	let warnWidth = ((warnRange[1] - warnRange[0]) / fullRange) * 10;
-	let warnStart = (warnRange[0] / fullRange) * 10;
+	let scaleValue = (value, range) => {
+		return ((value - range[0]) / (range[1] - range[0])) * 10;
+	};
 
-	let nominalWidth = ((nominalRange[1] - nominalRange[0]) / fullRange) * 10;
-	let nominalStart = (nominalRange[0] / fullRange) * 10;
+	let warnWidth = scaleValue(warnRange[1] - warnRange[0], [0, fullRange[1] - fullRange[0]]);
+	let warnStart = scaleValue(warnRange[0], fullRange);
 
-	let position = Math.min(Math.max((value / fullRange) * 10, 0), 10);
+	let nominalWidth = scaleValue(nominalRange[1] - nominalRange[0], [
+		0,
+		fullRange[1] - fullRange[0]
+	]);
+	let nominalStart = scaleValue(nominalRange[0], fullRange);
+
+	let position = Math.min(Math.max(scaleValue(value, fullRange), 0), 10);
 
 	console.log(warnWidth);
 </script>
